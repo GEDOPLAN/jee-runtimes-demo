51e8f1d6b0cbc587d6fa00b45b4b1f0a4aec34dd
Dirk Weil <dirk.weil@gedoplan.de>
2021-07-16 15:54:42 +0200

    Fix für Dev Mode (dev-watch)

---
+++ src/main/java/de/gedoplan/showcase/rest/HelloEndpoint.java
@@ -0,0 +1,18 @@
+package de.gedoplan.showcase.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("hello")
+public class HelloEndpoint {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String get() {
+    return "Hello!";
+  }
+}
